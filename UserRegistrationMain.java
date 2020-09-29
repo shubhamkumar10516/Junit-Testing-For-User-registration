7f302fba9041597e854bb1caccdf93df70037e2a
shubhamkumar10516 <33356820+shubhamkumar10516@users.noreply.github.com>
2020-09-30 02:47:35 +0530

    [Shubham]ADDED: UC2 checking last name added

--- UserRegistrationMain.java
+++ UserRegistrationMain.java
@@ -2,13 +2,13 @@ package com.bridgeLab.userReg;
 
 public class UserRegistrationMain {
 
-    public static boolean validateFirstName(String fname) {
+    public  boolean validateFirstName(String fname) {
     	
     	String pattern = "[A-Z]{1}[a-zA-Z]{2,}";
     	
     	return fname.matches(pattern);
     }
-    public static boolean validateLastName(String lname) {
+    public  boolean validateLastName(String lname) {
     	
 		String pattern = "[A-Z]{1}[a-zA-Z]{2,}";
     	
